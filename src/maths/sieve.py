cf5d684819756aa546a623170dada8ff9ee5377d
Amr Reda <amrreda@outlook.com>
2016-12-14 11:07:38 +1100

    incremental improvement

--- src/maths/sieve.py
+++ src/maths/sieve.py
@@ -6,10 +6,10 @@ __author__ = "AmrReda"
 
 def sieve(number):
     not_prime = []
-    for i in xrange(2, number+1):
+    for i in xrange(2, number + 1):
         if i not in not_prime:
             print i
-            for j in xrange(i*i, number+1, i):
+            for j in xrange(i * i, number + 1, i):
                 not_prime.append(j)
 
 sieve(100)
